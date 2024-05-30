75344b3438ef7f5a9cab065c2c87acab1250485f
KonstantinS <Konstantinruofficer@gmail.ru>
2024-05-30 10:10:19 +1000

    Первое задание решено

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-let a = "Kostya";
+// -------------------№ 1----------------
 
-alert(a);
-console.log(a);
+let a = 10;
+let b = 12;
+let sum = a + b;
+console.log(sum);
+
+// ----------
+
+let str1 = "Kostya";
+let str2 = "Sanin";
+let sumStr = str1.length + str2.length;
+console.log(sumStr);
+
+// ----------
+
+let inputNumber = prompt("Enter your number");
+let num1 = inputNumber[0];
+let num2 = inputNumber[1];
+let num3 = inputNumber[2];
+let sumNum = +num1 + +num2 + +num3;
+
+console.log(sumNum);
